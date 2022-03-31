b33bd02354d50ca061cce45ff372ba8430a05243
Dino Fajar <wattaw@outlook.com>
2022-04-01 02:18:55 +0700

    Inventory
    UI miss dikit2 + script invent udah tak copas dari file project sebelume

---
+++ Savile/Assets/Scripts/InventoryInput.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InventoryInput : MonoBehaviour
+{
+    [SerializeField] GameObject inventoryGameOject;
+    [SerializeField] KeyCode[] toggleInventoryKeys;
+
+    void Update()
+    {
+        for (int i = 0; i < toggleInventoryKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleInventoryKeys[i]))
+            {
+                inventoryGameOject.setActive(!inventoryGameOject.activeSelf);
+                break;
+            }
+        }    
+    }
+}
